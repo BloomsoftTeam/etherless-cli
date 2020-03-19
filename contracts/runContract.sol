78200a61c6860f471fc517bdbad9ca3c92e1b1de
Stefano Baggio <baggiostefano27@gmail.com>
2020-03-19 18:18:05 +0100

    Modifica runContract

--- contracts/runContract.sol
+++ contracts/runContract.sol
@@ -4,30 +4,33 @@ contract RunContract {
 
     // address payable ownerAddress;
     // uint basePrice;
+    mapping (string => address payable) private dev-fun;
+    mapping (string => uint) private fun-prices;
 
-    event runRequest(string fCode, string fParameters);
+    event runRequest(address payable fUser, string fName, string fParameters, address payable fDeveloper);
     //Se teniamo la funzione di somma, usiamo uint per mettergli i risultati
-    event runResult(uint fResult); //, uint remainingEthers);
+    event runResult(address payable rReceiver, string fResult); //, uint remainingEthers);
 
     // constructor () public{
     //     ownerAddress = msg.sender;
     //     //equivale a 0.05 ether
     //     basePrice = 50000000000000000 wei;
     // }
-    function getString() public view returns(string memory) {
+    function getString() public pure returns(string memory) {
         return "Hello";
     }
 
-    function sendRunEvent(string memory fCode, string memory fParameters) public{ 
+    function sendRunEvent(string memory fName, string memory fParameters, ) public{ 
         //Richiesta minima di basePrice sul wallet per eseguire l'operazione
         // require(ownerAddress.balance >= basePrice);
         //Trasferisce al contratto gli eth di basePrice dal wallet
         // ownerAddress.transfer(basePrice);
         //A questo punto si può procedere con la richiesta del run
-        emit runRequest(fCode, fParameters);
+        dev-fun[fName].transfer(fun-prices[fName]);
+        emit runRequest(msg.sender, fName, fParameters, dev-fun[fName]);
     }
 
-    function sendRunResult(uint fResult) //, uint runCost) payable 
+    function sendRunResult(address payable rReceiver, string fResult, uint moneyLeft) //, uint runCost) payable 
     public{
         //Calcola il resto
         // uint remainingEthers = basePrice - runCost;
@@ -38,6 +41,7 @@ contract RunContract {
         
 
         //Manda l'evento alla cli con risultato e resto di cui fare il console log
-        emit runResult(fResult); //, remainingEthers);
+        rReceiver.transfer(moneyLeft);
+        emit runResult(rReceiver, fResult); //, remainingEthers);
     }
-} 
+}
